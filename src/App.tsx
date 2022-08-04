03a7a090f5b45ca8954db9a24d668a65ab065bf9
rutyCohen <213235542@mby.co.il>
2022-08-04 19:21:21 +0300

    connect between client and nest

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useReducer, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  interface user{
+    name: string;
+    age: number;
+    phone: string;
+  }
+  const [users, setUsers] = useState<user[]>([]);
+  useEffect(() => {
+    const fetch = async () => {
+        try {
+            const res = await axios.get(`http://localhost:3333/user`);
+            setUsers(res.data);
+        } catch (e) {
+            alert(e);
+        }
+    };
+    fetch();
+}, []);
+  return (
+    <>
+    <div className="App">
+     HELLO OUR USERS
+    </div>
+    {users && 
+    users.map((u) => (
+      <div>
+          <li>{u.name}</li>
+      </div>
+  ))}
+    </>
+  );
+}
+
+export default App;
