9454f6c8db7d0dd04d520a9d395828c0181aaa17
RuchamiBerenshtein <r9743740@gmail.com>
2022-08-10 10:08:42 +0300

    try to build a firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import fire from './Fire'
 import './App.css';
 
 function App() {
-  interface user{
+  interface user {
     name: string;
     age: number;
     phone: string;
@@ -11,28 +12,34 @@ function App() {
   const [users, setUsers] = useState<user[]>([]);
   useEffect(() => {
     const fetch = async () => {
-        try {
-            const res = await axios.get(`http://localhost:3333/user`);
-            setUsers(res.data);
-        } catch (e) {
-            alert(e);
-        }
+      try {
+        const res = await axios.get(`http://localhost:3333/user`);
+        console.log(res.data);
+        setUsers(res.data);
+      } catch (e) {
+        alert(e);
+      }
     };
     fetch();
-}, []);
+  }, []);
+  fire.auth().onAuthStateChange((user: any) => console.log(user));
+
   return (
     <>
-    <div className="App">
-     HELLO OUR USERS
-    </div>
-    {users && 
-    users.map((u) => (
-      <div>
-          <li>{u.name}</li>
+      <div className="App">
+        HELLO OUR USERS
       </div>
-  ))}
+      {users &&
+        users.map((u, index) => (
+          <div>
+            <li key={index}>{u.name}</li>
+          </div>
+        ))}
     </>
   );
 }
 
 export default App;
+
+
+
