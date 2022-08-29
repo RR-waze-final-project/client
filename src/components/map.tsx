1666ae9761d9044fae4a26645a8b016bc8a0ea20
rutyCohen <213235542@mby.co.il>
2022-08-29 12:08:29 +0300

    add marker

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,27 +1,74 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 // useCallback, useState, useRef,
 import { GoogleMap } from '@react-google-maps/api';
+import GoogleMapReact from 'google-map-react';
 // , Marker, DirectionsRenderer, Circle, MarkerClusterer
 // type gml = google.maps.LatLngLiteral;
 // type gmd = google.maps.DirectionsResult;
 // type gmm = google.maps.MapOptions;
 import '../style/map.css';
+import Marker from './Marker';
+
 
 export const Map = () => {
+    // const getMapOptions = (maps: any) => {
+    //     return {
+    //       disableDefaultUI: true,
+    //       mapTypeControl: true,
+    //       streetViewControl: true,
+    //       styles: [{ featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }],
+    //     };
+    //   };
 
-    const center = useMemo(() => ({
-        lat: 31.75,
-        lng: 35.2
-    }), []);
+    // const center = useMemo(() => ({
+    //     lat: 31.75,
+    //     lng: 35.2
+    // }), []);
 
-    return (
-        <div>
-            <GoogleMap
-                zoom={12}
-                center={center}
-                mapContainerClassName='map-container'
-            ></GoogleMap>
-        </div>
-    )
+    // return (
+    //     <div>
+    //         <GoogleMap
+    //             zoom={12}
+    //             center={center}
+    //             mapContainerClassName='map-container'
+    //             options={getMapOptions}
+    //         >
+    //         <Marker
+    //             lat={11.0168}
+    //             lng={76.9558}
+    //             name="My Marker"
+    //             color="blue"
+    //         />
+    //         </GoogleMap>
+    //     </div>
+    // )
+    const getMapOptions = (maps: any) => {
+        return {
+          disableDefaultUI: true,
+          mapTypeControl: true,
+          streetViewControl: true,
+          styles: [{ featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }],
+        };
+      };
+    
+        const [center, setCenter] = useState({lat: 31.75, lng: 35.2 });
+        const [zoom, setZoom] = useState(11);
+        return (
+            <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: 'replace your api key' }}
+              defaultCenter={center}
+              defaultZoom={zoom}
+              options={getMapOptions}
+            >
+              <Marker
+                lat={31.75}
+                lng={35.2}
+                name="My Marker"
+                color="red"
+              />
+            </GoogleMapReact>
+          </div>
+        );
 }
 
--- src/components/systemHome.tsx
+++ src/components/systemHome.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { SystemHeader } from './systemHeader';
-import { Maps } from './maps';
+import { Map } from './map';
 import { Box } from '@mui/material';
-import { SearchHeader } from './searchHeader';
 import { AutoComplete } from './autoComplete';
+import { useLoadScript } from '@react-google-maps/api';
+
 
 export const SystemHome = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyADGRXFAQJjRYgjE_UBoOpTp1XWFlpqBu0',
+    libraries: ['places']
+  })
 
+  if (!isLoaded) {
+    return <div>loading...</div>
+  }
   return (
     <div>
       <Box sx={{textAlign: 'center'}}>
@@ -14,7 +22,7 @@ export const SystemHome = () => {
       </Box>
       <Box sx={{width: '100%', display: 'flex'}}>
         <Box sx={{width: '80%'}}>
-          <Maps />
+          <Map />
         </Box>
         <Box sx={{width: '20%', direction: 'rtl'}}>
           <AutoComplete />
